04f188bfd150e8ad3bacfaa67adaecc41ed36f1c
vincentjessica30 <vincentjessica30@gmail.com>
2026-03-28 12:23:25 -0700

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const mobileNav = document.querySelector('.mobile-nav');
-
-menuToggle.addEventListener('click', function () {
-  mobileNav.classList.toggle('open');
-});
